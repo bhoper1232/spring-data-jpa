976900c6eba747c5a53512cea48c675340a6a2fc
Andrii Solianyk <solianik123456@gmail.com>
2023-07-22 16:59:02 +0200

    First commit

---
+++ src/main/java/com/andrii/spring/springboot/spring_data_jpa/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.andrii.spring.springboot.spring_data_jpa.service;
+
+
+
+import com.andrii.spring.springboot.spring_data_jpa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+
+    List<Employee> findAllByName(String name);
+
+}
